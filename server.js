58acf8045fe7834b3dfa0471255c8bc354ac03cf
Tim Shaker <tim@tmshkr.com>
2022-07-22 17:53:13 -0700

    check magic_word

--- server.js
+++ server.js
@@ -9,4 +9,33 @@ const handler = app.getRequestHandler();
   });
 })();
 
-module.exports.handleHTTP = handler;
+module.exports.handleHTTP = (req, res) => {
+  const cookies = parseCookies(req);
+  const magic_word = req.query.magic_word || cookies.magic_word;
+
+  if (magic_word !== cookies.magic_word) {
+    res.cookie("magic_word", magic_word);
+  }
+  if (magic_word !== process.env.MAGIC_WORD) {
+    return res.status(401).send("Unauthorized");
+  }
+
+  handler(req, res);
+};
+
+function parseCookies(request) {
+  const list = {};
+  const cookieHeader = request.headers?.cookie;
+  if (!cookieHeader) return list;
+
+  cookieHeader.split(`;`).forEach(function (cookie) {
+    let [name, ...rest] = cookie.split(`=`);
+    name = name?.trim();
+    if (!name) return;
+    const value = rest.join(`=`).trim();
+    if (!value) return;
+    list[name] = decodeURIComponent(value);
+  });
+
+  return list;
+}
